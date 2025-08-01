490e22310d29006c6187f2099ba1eef1c7806842
mariaprzylucka <maria.przylucka@pgi.gov.pl>
2025-08-01 14:20:12 +0200

    Update imgw_collector.py

--- imgw_collector.py
+++ imgw_collector.py
@@ -13,7 +13,7 @@ from datetime import datetime
 URL = "https://danepubliczne.imgw.pl/api/data/meteo/"
 
 def get_data():
-    response = requests.get(URL)
+    response = requests.get(URL, stream=True)
     
     if response.status_code == 200:
         data = response.json()
