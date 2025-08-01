ef32e0686672fd71c0b11c97bb4b9412307c11bf
mariaprzylucka <maria.przylucka@pgi.gov.pl>
2025-08-01 12:00:01 +0200

    Add files via upload

---
+++ imgw_collector.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  1 11:56:28 2025
+
+@author: msur
+"""
+
+import requests
+import json
+from datetime import datetime
+
+URL = "https://danepubliczne.imgw.pl/api/data/meteo/"
+
+def get_data():
+    response = requests.get(URL, stream=True)
+    
+    if response.status_code == 200:
+        data = response.json()
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%SZ')
+        filename = f"data/imgw_data_{timestamp}.json"
+        
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+        print(f"Saved: {filename}")
+    else:
+        print(f"Failed to fetch data. Status: {response.status_code}")
+
+if __name__ == "__main__":
+    get_data()
